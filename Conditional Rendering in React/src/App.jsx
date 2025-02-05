1e90f12c85fd578fde3551485f4c8d5a0ec4c034
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-02-06 03:37:21 +0600

    changes

--- Conditional Rendering in React/src/App.jsx
+++ Conditional Rendering in React/src/App.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import LoginBtn from './components/LoginBtn';
+import LogoutBtn from './components/LogoutBtn';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  if (!isLoggedIn) {
+    return (
+      <LoginBtn />
+    )
+  }
 
   return (
-    <>
+    <div>
+      <h1>welcome to React JS</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {isLoggedIn && <LogoutBtn />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
+  //// ............................................................................................
+  // return (
+  //   <div>
+  //     {isLoggedIn ? <LogoutBtn /> : <LoginBtn />}
+  //   </div>
+  // )
+  //// ............................................................................................
+  // if (isLoggedIn) {
+  //   return (
+  //     <LogoutBtn />
+  //   )
+  // }
+  // else {
+  //   return (
+  //     <LoginBtn />
+  //   )
+  // }
 }
 
 export default App
