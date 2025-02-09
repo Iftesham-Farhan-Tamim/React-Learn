719f99ad54fefab4f2fff9acb114d28435a5ed91
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-02-10 04:01:32 +0600

    update

--- Routing/src/components/Router.jsx
+++ Routing/src/components/Router.jsx
@@ -10,65 +10,67 @@ import Reports from './child/Reports';
 import NotFound from './NotFound';
 
 // Define the router configuration
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: (
-            <div>
-                <Navbar />
-                <Home />
-            </div>
-        ),
-    },
+const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: (
+                <div>
+                    <Navbar />
+                    <Home />
+                </div>
+            ),
+        },
 
-    {
-        path: '/about',
-        element: (
-            <div>
-                <Navbar />
-                <About />
-            </div>
-        ),
-    },
+        {
+            path: '/about',
+            element: (
+                <div>
+                    <Navbar />
+                    <About />
+                </div>
+            ),
+        },
 
-    {
-        path: '/dashboard',
-        element: (
-            <div>
-                <Navbar />
-                <DashBoard />
-            </div>
-        ),
-        children: [
-            {
-                path: 'courses',
-                element: <Courses />,
-            },
-            {
-                path: 'mock-tests',
-                element: <MockTest />,
-            },
-            {
-                path: 'reports',
-                element: <Reports />,
-            },
-        ],
-    },
+        {
+            path: '/dashboard',
+            element: (
+                <div>
+                    <Navbar />
+                    <DashBoard />
+                </div>
+            ),
+            children: [
+                {
+                    path: 'courses',
+                    element: <Courses />,
+                },
+                {
+                    path: 'mock-tests',
+                    element: <MockTest />,
+                },
+                {
+                    path: 'reports',
+                    element: <Reports />,
+                },
+            ],
+        },
 
-    {
-        path: '/student/:id',
-        element: (
-            <div>
-                <Navbar />
-                <ParamComp />
-            </div>
-        ),
-    },
+        {
+            path: '/student/:id',
+            element: (
+                <div>
+                    <Navbar />
+                    <ParamComp />
+                </div>
+            ),
+        },
 
-    {
-        path: '*',
-        element: <NotFound />,
-    },
-]);
+        {
+            path: '*',
+            element: <NotFound />,
+        },
+    ]
+);
 
 export default router;
