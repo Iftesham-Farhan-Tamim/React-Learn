820bc5be1ddf024a8b97a51358f2c15d88198754
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-02-11 12:17:28 +0600

    adding files

--- Redux Toolkit/src/App.jsx
+++ Redux Toolkit/src/App.jsx
@@ -1,34 +1,47 @@
+// npm install react-redux
+// npm install @reduxjs/toolkit
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, reset, incrementByAmount } from './features/counter/counterSlice';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [amount, setAmount] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleIncrementClick() {
+    dispatch(increment());
+  }
+
+  function handleDecrementClick() {
+    dispatch(decrement());
+  }
+
+  function handleResetClick() {
+    dispatch(reset());
+  }
+
+  function handleIncAmountClick() {
+    dispatch(incrementByAmount(amount));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <button onClick={handleIncrementClick}>+</button>
+      <p>Counrt: {count}</p>
+      <button onClick={handleDecrementClick}>-</button> <br /> <br />
+      <button onClick={handleResetClick}>Reset</button> <br /> <br />
+      <input
+        type='Number'
+        value={amount}
+        placeholder='Enter Amount'
+        onChange={(e) => setAmount(e.target.value)}
+      /> <br /> <br />
+      <button onClick={handleIncAmountClick}>Inc by Amount</button>
+    </div>
   )
 }
 
