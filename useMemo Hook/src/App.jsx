65bf9cdb4335140e5b7d84659e4e13257bfffeb4
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-02-12 12:57:25 +0600

    adding files

--- useMemo Hook/src/App.jsx
+++ useMemo Hook/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useMemo } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
+
+  function handleCount() {
+    setCount(count + 1);
+  }
+
+  function expensiveTask(num) {
+    console.log("Inside Expensive Task");
+    for (let i = 0; i <= 1000000000; i++) { }
+    return num * 2;
+  }
+
+  let doubleValue = useMemo(() => expensiveTask(input), [input])
 
   return (
-    <>
+    <div>
+      <button onClick={handleCount}>
+        Increment
+      </button> <br /> <br />
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        Count: {count}
+      </div> <br /> <br />
+
+      <input
+        type="number"
+        placeholder='enter number'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <div>
+        Double: {doubleValue}
+      </div> <br /> <br />
+    </div>
   )
 }
 
